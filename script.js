f794b03adf33e58143da212d3d05be9891d9084f
Allan Constanza <ajconstanzaaguirre@csuchico.edu>
2024-09-24 00:59:57 -0700

    Modifications to App

--- script.js
+++ script.js
@@ -10,11 +10,13 @@ const weatherCardsDiv = document.querySelector(".weather-cards");
 
 const API_KEY = 'be3f32faa630726083bb780f345e23fd'; // API key for OpenWeatherMap API
 
+
 const createWeatherCard = (cityName, weatherItem, index) => {
+    const temperatureF = ((weatherItem.main.temp - 273.15) * (9/5) + 32).toFixed(2); // Convert to Fahrenheit
     if(index === 0) { // HTML for the main weather card
         return `<div class="details">
                     <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-                    <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Temperature: ${temperatureF}°F</h6> <!-- Change here -->
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
                 </div>
@@ -26,7 +28,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
         return `<li class="card">
                     <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Temp: ${temperatureF}°F</h6> <!-- Change here -->
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
                 </li>`;
